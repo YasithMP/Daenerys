5f20ba62bea276530d9da75a03980be446e96e0a
Yasith Perera <121958251+YasithMP@users.noreply.github.com>
2024-08-15 10:03:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+from apikeys import BotToken
+
+
+
+class Dany(commands.Bot):
+
+    def __init__(self):
+        super().__init__(
+            command_prefix='.',
+            intents=discord.Intents.all()
+            )
+    
+    async def setup_hook(self) -> None: 
+        await self.load_extension(f"cogs.ping")
+        await self.load_extension(f"cogs.snipe")
+        await self.load_extension(f"cogs.flip")
+        await self.load_extension(f"cogs.av")
+        await client.tree.sync()
+
+    async def on_ready(self):
+        print(f"{self.user} has connected to Discord")
+
+
+
+client = Dany()
+client.run(BotToken)
