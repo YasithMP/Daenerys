5a20d7d786c052fa1935db10e5064dfc8e61ff65
Yasith Perera <yasithperera294@gmail.com>
2025-05-25 15:11:45 +0530

    Adds prefix command for flip

--- cogs/flip.py
+++ cogs/flip.py
@@ -19,6 +19,16 @@ class flip(commands.Cog):
 
         await interaction.response.send_message(coin_flip)
 
+    @commands.command()
+    async def flip(self, ctx):
+        result = random.randint(1, 2)
+        if result == 1:
+            coin_flip = "Heads"
+        else:
+            coin_flip = "Tails"
+
+        await ctx.reply(coin_flip)
+
 async def setup(client: commands.Bot) -> None:
     await client.add_cog(flip(client))
             
