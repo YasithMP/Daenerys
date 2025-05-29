19318d368f97ccc26ec635de7b6bd2f29f10944f
Yasith Perera <yasithperera294@gmail.com>
2025-05-29 14:19:29 +0530

    adds prefix command for snipe

--- cogs/snipe.py
+++ cogs/snipe.py
@@ -14,7 +14,7 @@ class Snipe(commands.Cog):
         self.client.sniped_messages[message.guild.id] = (message.content, message.author, message.channel.name, message.created_at)
 
     @app_commands.command(name="snipe", description="Recovers a deleted message")
-    async def snipe(self, interaction: discord.Interaction):
+    async def slash_snipe(self, interaction: discord.Interaction):
 
         try:
             contents, author, channel_name, time = self.client.sniped_messages[interaction.guild.id]
@@ -30,6 +30,23 @@ class Snipe(commands.Cog):
                 await interaction.response.send_message("Can't find anything.")
         except Exception:
             await interaction.response.send_message("Can't find anything.")
+
+    @commands.command(name="snipe")
+    async def prefix_snipe(self, ctx):
+        try:
+            contents, author, channel_name, time = self.client.sniped_messages[ctx.guild.id]
+
+            if (ctx.channel.name == channel_name):
+
+                embed = discord.Embed(description=contents, color=discord.Color.random(), timestamp=time)
+                embed.set_author(name=f"Message sent by {author.name}", icon_url=author.avatar.url)
+                embed.set_footer(text=f"Deleted in : #{channel_name}")
+                await ctx.reply(embed=embed)
+
+            else:
+                await ctx.reply("Can't find anything.")
+        except Exception:
+            await ctx.reply("Can't find anything.")
 
 
 
